5dcd08aa0eefc1c1aa635712b4f062e36a37bf5d
ghosts-arch <ronan.questier@mailfence.com>
2026-07-30 13:40:46 +0200

    start add app style

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,16 @@
 <script lang="ts">
+    import "@picocss/pico";
     let { children } = $props();
 </script>
 
-{@render children()}
+<header class="container">
+    <nav>
+        <ul>
+            <li><a href="/assets/create">Ajouter un asset</a></li>
+            <li><a href="/assets">liste des assets</a></li>
+        </ul>
+    </nav>
+</header>
+<main class="container">
+    {@render children()}
+</main>
